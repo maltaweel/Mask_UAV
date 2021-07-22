7a89606ec8e3d57a7615e73bccb6854c5bf9a0fd
maltaweel <m.altaweel@ucl.ac.uk>
2021-07-22 11:57:39 +0100

    commit

---
+++ src/gui/segment_gui.py
@@ -0,0 +1,63 @@
+'''
+Created on Jul 22, 2021
+
+@author: maltaweel
+'''
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QInputDialog, QLineEdit
+
+from gui import Gui
+from custome_segmentation import segmentation as segment
+
+class SegmentGUI(Gui):
+    
+    def makeSegmentingButtons(self, app):
+        window = QWidget()
+        self.layout=QVBoxLayout()
+        widgets=['Segment Image','Weight File']
+        self.makeWidgetPushButton(widgets,app)
+        
+        btn = QPushButton('Input Class Name(s)')
+        btn.toggle()
+        btn.clicked.connect(self.showDialog)
+        self.layout.addWidget(btn)
+        
+        self.startButton(self.layout)
+        window.setLayout(self.layout)
+        window.show()
+        app.exec()
+
+        
+    def showDialog(self):
+        text1, ok1=QInputDialog.getText(self, "Get text","Class Name (comma seperated)", QLineEdit.Normal, "")
+
+        
+        if ok1:
+            splits=text1.split(',')
+            self.classNumber=len(splits)
+            self.classes=splits
+
+            
+    def doActionOne(self):
+        qid = QFileDialog()
+        location=str(QFileDialog.getOpenFileName()[0])
+        self.image=location
+    
+    def doActionTwo(self):
+        qid = QFileDialog()
+        location=str(QFileDialog.getOpenFileName()[0])
+        self.weight=location
+    
+    def start(self):
+        segment.startSegmenting(self.image, self.weight,self.classNumber,self.classes)
+        
+        
+def main():
+
+    app = QApplication([])
+    sg=SegmentGUI()
+    sg.makeSegmentButtons(app)
+    sys.exit(app.exec_())
+    
+if __name__ == '__main__':
+    main()
--- src/segmentation/custom_segmentation.py
+++ src/segmentation/custom_segmentation.py
@@ -8,33 +8,51 @@ import csv
 import pixellib
 from pixellib.instance import custom_segmentation
 
-#path 
 pn=os.path.abspath(__file__)
 pn=pn.split("src")[0]
-path=os.path.join(pn,'structures','test','The Iron Age Fort at Jebel Buhais, imaged by an ex-drone.JPG')
-model_path=os.path.join(pn,'model_dir')
-weights_path=os.path.join(pn,'weights','mask_rcnn_model.001-1.412928.h5')
-output_segmentation=os.path.join(pn,'output_segmentation','segmented.jpg')
-seg_out=os.path.join(pn,'output_segmentation','segment_data.csv')
 
-#segment
-segment_image = custom_segmentation()
-segment_image.inferConfig(num_classes= 1, class_names= ["BG", 'ruined structure'])
-segment_image.load_model(weights_path)
-segmask, output=segment_image.segmentImage(path, show_bboxes=True, output_image_name=output_segmentation,
-extract_segmented_objects= True, save_extracted_objects=False)
+def startSegmenting(image,weight,classNumber,classes):
+    #path info
+    path=image
+    #model_path=os.path.join(pn,'model_dir')
+    weights_path=weight
+    output_segmentation=os.path.join(pn,'output_segmentation','segmented.jpg')
+    seg_out=os.path.join(pn,'output_segmentation','segment_data.csv')
 
-res = segmask["extracted_objects"]
-scores=segmask['scores']
-ids=segmask['class_ids']
+    #segment
+    segment_image = custom_segmentation()
+    
+    addClasses=[]
+    addClasses.append("BG")
+    for i in classes:
+        addClasses.append(i)
+        
+    segment_image.inferConfig(num_classes= classNumber, class_names=addClasses)
+    segment_image.load_model(weights_path)
+    segmask, output=segment_image.segmentImage(path, show_bboxes=True, output_image_name=output_segmentation,
+            extract_segmented_objects= True, save_extracted_objects=False)
 
-fieldnames=['id','a','b','c','score']
-with open(seg_out, 'w') as csvf:
-    #write the output
-    writer = csv.DictWriter(csvf, fieldnames=fieldnames)
+    outputData(segmask,seg_out)
+    
+def outputData(segmask,seg_out):
+    res = segmask["extracted_objects"]
+    scores=segmask['scores']
+    ids=segmask['class_ids']
 
-    writer.writeheader() 
+    fieldnames=['id','a','b','c','score']
+    with open(seg_out, 'w') as csvf:
+        #write the output
+        writer = csv.DictWriter(csvf, fieldnames=fieldnames)
 
-    for i in range(0,len(res)):
-        writer.writerow({'id':str(ids[i]),'a': str(res[i].shape[0]),'b':str(res[i].shape[1]),'c':str(res[i].shape[2]),
+        writer.writeheader() 
+
+        for i in range(0,len(res)):
+            writer.writerow({'id':str(ids[i]),'a': str(res[i].shape[0]),'b':str(res[i].shape[1]),'c':str(res[i].shape[2]),
                          'score':str(scores[i])})
+        
+if __name__ == "__main__":
+    image=os.path.join(pn,'structures','test','Burials-APAAME_20100601_SES-0108.jpg')
+    weight_location=os.path.join(pn,'weights','mask_rcnn_model.001-1.412928.h5')
+    
+    startSegmenting(image,weight_location,1,['BG','ruined structure'])
+    
