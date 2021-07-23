c7da0e4913e231bdfad5657027538a859f6b8cb4
maltaweel <m.altaweel@ucl.ac.uk>
2021-07-23 14:12:26 +0100

    commit

--- src/training/train_set.py
+++ src/training/train_set.py
@@ -4,6 +4,7 @@ Created on Mar 27, 2021
 @author: maltaweel
 '''
 import os
+import csv
 import pixellib
 from pixellib.custom_train import instance_custom_training
 
@@ -25,13 +26,37 @@ def startTraining(training_location,weight_location, batch, classN,network,epoch
     #train_maskrcnn.visualize_sample()
     train_maskrcnn.train_model(num_epochs = epoch, augmentation=True,  path_trained_models = model_path)
 
+def loadTraining():
+    pn=os.path.abspath(__file__)
+    pn=pn.split("src")[0]
+    
+    f=os.path.join(pn,'training_data','training_data.csv')
+    
+    #open files
+    with open(f,'r',encoding="ISO-8859-1") as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        #Training Location,Weight Location,Batch Number,Class Number,Network Model,Epochs
+        for row in reader:
+                    
+            #get text
+            tlocation=row['Training Location']
+            wlocation=row['Weight Location']
+            bn=int(row['Batch Number'])
+            cn=int(row['Class Number'])
+            nModel=row['Network Model']
+            epochs=int(row['Epochs'])
+     
+    return tlocation,wlocation,bn,cn,nModel,epochs       
+
 if __name__ == "__main__":
     # use this for HPC training
-    #first setup paths for training and weight
-    training_location=os.path.join(pn,'structures')
-    weight_location=os.path.join(pn,'weights','mask_rcnn_coco.h5')
+    #first setup paths for training and weight 
+    #and other details from input file
+
+    tlocation,wlocation,bn,cn,nModel,epochs=loadTraining()
     
     #do the training
-    startTraining(training_location,weight_location,4,3,'resnet101',300)
+    startTraining(tlocation,wlocation,bn,cn,nModel,epochs)
     
     print("Finished") 
