4cbe29bb230e60937136fc86afbdc433ff8e310a
maltaweel <m.altaweel@ucl.ac.uk>
2021-08-12 19:47:24 +0100

    commit

---
+++ controller.py
@@ -0,0 +1,23 @@
+'''
+Created on Jul 26, 2021
+
+@author: maltaweel
+'''
+import easygui
+import gui.train_gui as train
+import gui.segment_gui as segment
+import labelme.__main__ as labelMe
+
+def main():
+    result=easygui.buttonbox('Click on your choice.', 'Options', ('Annotation','Training','Segmentation'))
+    
+    if result=='Training':
+        train.main()
+    elif result=='Segmentation':
+        segment.main()
+    else:
+        labelMe.main()
+    
+if __name__ == '__main__':
+    main()
+        
